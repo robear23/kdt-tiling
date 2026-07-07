807e7cbdfb72d642b8ef8e4b6ee309e9dc3c3c32
Robbie <robsage96@hotmail.com>
2026-07-07 13:01:49 +0200

    Remove (Optional) label from email field in contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -253,7 +253,7 @@ export default function Contact() {
                   htmlFor="email"
                   className="absolute text-base text-gray-400 duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-electric-cyan peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Email Address (Optional)
+                  Email Address
                 </label>
               </div>
 
